8c46ee68ddac7ac47d3853924b1f8fe0c367e5f5
Arthur Chiao <16827192+acchiao@users.noreply.github.com>
2023-03-26 21:40:49 -0700

    Initial commit

---
+++ truss/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from truss.users.models import User
+from truss.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
